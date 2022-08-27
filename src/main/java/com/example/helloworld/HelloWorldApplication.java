b937c04b7ac95a26e939fa6f749c26fab7b79673
snehapal2012 <snehapal2012@gmail.com>
2022-08-27 17:00:30 +0530

    Modified: create web page to show a message

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -9,6 +9,7 @@ public class HelloWorldApplication {
     public static void main(String[] args) {
         System.out.println("Hello from BridgeLabz !!!!!!");
         SpringApplication.run(HelloWorldApplication.class, args);
+
     }
 
 }
